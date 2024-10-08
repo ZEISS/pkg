adb2a368747d7757ade4f9075ec31b86f6cd66c0
Sebastian Döll <sebastian@katallaxie.dev>
2024-10-08 21:32:31 +0000

    feat: add pg config fix: remove internal tools

---
+++ dbx/pg/config.go
@@ -0,0 +1,67 @@
+package pg
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/zeiss/pkg/utilx"
+)
+
+// Config represents configuration for PostgreSQL connection
+type Config struct {
+	Database string `envconfig:"PG_DB_NAME"`
+	Host     string `envconfig:"PG_HOST"`
+	Password string `envconfig:"PG_PASSWORD"`
+	Port     int    `envconfig:"PG_PORT" default:"5432"`
+	SslMode  string `envconfig:"PG_SSL_MODE" default:"disable"`
+	User     string `envconfig:"PG_USER"`
+}
+
+// NewConfig returns a new Config instance
+func NewConfig() *Config {
+	return &Config{
+		Port:    5432,
+		SslMode: "disable",
+	}
+}
+
+// FormatDSN formats the given Config into a DSN string which can be passed to the driver.
+func (c Config) FormatDSN() string {
+	var params []string
+
+	if utilx.NotEmpty(c.Database) {
+		params = append(params, "dbname="+c.escape(c.Database))
+	}
+
+	if utilx.NotEmpty(c.User) {
+		params = append(params, "user="+c.escape(c.User))
+	}
+
+	if utilx.NotEmpty(c.Password) {
+		params = append(params, "password="+c.escape(c.Password))
+	}
+
+	if utilx.NotEmpty(c.Host) {
+		params = append(params, "host="+c.escape(c.Host))
+	}
+
+	if utilx.NotEmpty(c.Port) {
+		params = append(params, "port="+strconv.Itoa(c.Port))
+	}
+
+	if utilx.NotEmpty(c.SslMode) {
+		params = append(params, "sslmode="+c.escape(c.SslMode))
+	}
+
+	return strings.Join(params, " ")
+}
+
+func (c *Config) escape(str string) string {
+	if !strings.Contains(str, " ") {
+		return str
+	}
+
+	str = strings.ReplaceAll(str, "'", "\\'")
+	return fmt.Sprintf("'%s'", str)
+}
---
+++ dbx/pg/config_test.go
@@ -0,0 +1,35 @@
+package pg_test
+
+import (
+	"testing"
+
+	"github.com/zeiss/pkg/dbx/pg"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewConfig(t *testing.T) {
+	t.Parallel()
+
+	config := pg.NewConfig()
+	require.NotNil(t, config)
+	assert.Equal(t, 5432, config.Port)
+	assert.Equal(t, "disable", config.SslMode)
+}
+
+func TestFormatDSN(t *testing.T) {
+	t.Parallel()
+
+	config := &pg.Config{
+		Database: "test_db",
+		Host:     "localhost",
+		Password: "password",
+		Port:     5432,
+		SslMode:  "disable",
+		User:     "test_user",
+	}
+
+	dsn := config.FormatDSN()
+	assert.Equal(t, "dbname=test_db user=test_user password=password host=localhost port=5432 sslmode=disable", dsn)
+}
