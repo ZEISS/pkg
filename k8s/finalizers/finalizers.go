e96d86849465c4f29adb7c4f7b7009322d9f70ac
Sebastian Döll <sebastian@katallaxie.dev>
2024-11-20 09:26:24 +0000

    feat: add finalizers

---
+++ k8s/finalizers/finalizers.go
@@ -0,0 +1,42 @@
+
+package finalizers
+
+// Finalizer is an interface that can be used to add, remove and check for
+type Finalizer interface {
+	GetFinalizers() []string
+}
+
+// AddFinalizer ...
+func AddFinalizer(obj Finalizer, finalizer string) []string {
+	finalizers := obj.GetFinalizers()
+	for _, f := range finalizers {
+		if finalizer == f {
+			return finalizers
+		}
+	}
+
+	return append([]string{finalizer}, finalizers...)
+}
+
+// HasFinalizer ...
+func HasFinalizer(obj Finalizer, finalizer string) bool {
+	finalizers := obj.GetFinalizers()
+	for _, f := range finalizers {
+		if finalizer == f {
+			return true
+		}
+	}
+	return false
+}
+
+// RemoveFinalizer ...
+func RemoveFinalizer(obj Finalizer, finalizer string) []string {
+	finalizers := []string{}
+	for _, f := range obj.GetFinalizers() {
+		if f == finalizer {
+			continue
+		}
+		finalizers = append(finalizers, f)
+	}
+	return finalizers
+}
