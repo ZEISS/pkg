f028baf0aa4985da7f024ca850498df0e7d6bda5
Sebastian Döll <sebastian@katallaxie.dev>
2024-11-21 14:50:53 +0000

    feat: add reconciler events

---
+++ k8s/reconciler/deployment.go
@@ -0,0 +1,62 @@
+package reconciler
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Event leverages go's 1.13 error wrapping.
+type Event error
+
+// EventIs reports whether any error in err's chain matches target.
+var EventIs = errors.Is
+
+// EventAs finds the first error in err's chain that matches target, and if so,
+// sets target to that error value and returns true.
+var EventAs = errors.As
+
+// NewEvent returns a new Event.
+func NewEvent(eventtype, reason, messageFmt string, args ...interface{}) Event {
+	return &ReconcilerEvent{
+		EventType: eventtype,
+		Reason:    reason,
+		Format:    messageFmt,
+		Args:      args,
+	}
+}
+
+// ReconcilerEvent ...
+type ReconcilerEvent struct { //nolint:errname
+	EventType string
+	Reason    string
+	Format    string
+	Args      []interface{}
+}
+
+// make sure ReconcilerEvent implements error.
+var _ error = (*ReconcilerEvent)(nil)
+
+// Is ...
+func (e *ReconcilerEvent) Is(target error) bool {
+	var t *ReconcilerEvent
+	if errors.As(target, &t) {
+		if t != nil && t.EventType == e.EventType && t.Reason == e.Reason {
+			return true
+		}
+		return false
+	}
+	// Allow for wrapped errors.
+	err := fmt.Errorf(e.Format, e.Args...)
+	return errors.Is(err, target)
+}
+
+// As ...
+func (e *ReconcilerEvent) As(target interface{}) bool {
+	err := fmt.Errorf(e.Format, e.Args...)
+	return errors.As(err, target)
+}
+
+// Error ...
+func (e *ReconcilerEvent) Error() string {
+	return fmt.Errorf(e.Format, e.Args...).Error()
+}
