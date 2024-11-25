69a876fd709683cdba3405f6e7034cda78567af1
Sebastian Döll <sebastian@katallaxie.dev>
2024-11-25 09:53:56 +0000

    doc: update finalizers docs

--- k8s/finalizers/finalizers.go
+++ k8s/finalizers/finalizers.go
@@ -1,41 +1,48 @@
 package finalizers
 
+import "github.com/zeiss/pkg/slices"
+
 // Finalizer is an interface that can be used to add, remove and check for
 type Finalizer interface {
 	GetFinalizers() []string
 }
 
-// AddFinalizer ...
+// AddFinalizer is a helper function to add a finalizer
 func AddFinalizer(obj Finalizer, finalizer string) []string {
 	finalizers := obj.GetFinalizers()
+
 	for _, f := range finalizers {
 		if finalizer == f {
 			return finalizers
 		}
 	}
 
-	return append([]string{finalizer}, finalizers...)
+	return slices.Append([]string{finalizer}, finalizers...)
 }
 
-// HasFinalizer ...
+// HasFinalizer is a helper function to check if a finalizer is present
 func HasFinalizer(obj Finalizer, finalizer string) bool {
 	finalizers := obj.GetFinalizers()
+
 	for _, f := range finalizers {
 		if finalizer == f {
 			return true
 		}
 	}
+
 	return false
 }
 
-// RemoveFinalizer ...
+// RemoveFinalizer is a helper function to remove a finalizer
 func RemoveFinalizer(obj Finalizer, finalizer string) []string {
 	finalizers := []string{}
+
 	for _, f := range obj.GetFinalizers() {
 		if f == finalizer {
 			continue
 		}
-		finalizers = append(finalizers, f)
+		finalizers = slices.Append(finalizers, f)
 	}
+
 	return finalizers
 }
