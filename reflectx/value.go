7cb23db8198970c76647c6492a280b4922ce9962
Sebastian Döll <sebastian@katallaxie.dev>
2025-01-15 22:33:10 +0000

    feat(reflectx): add is empty value

---
+++ reflectx/value.go
@@ -0,0 +1,17 @@
+package reflectx
+
+import "reflect"
+
+func IsEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64,
+		reflect.Interface, reflect.Pointer:
+		return v.IsZero()
+	}
+	return false
+}
